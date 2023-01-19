725ee7277e2a00a9713b8bbc5132b46ef141ffb5
ashok-5 <ak05091881@gmail.com>
2023-01-19 23:52:28 +0530

    done

--- prt.js
+++ prt.js
@@ -1,6 +1,6 @@
-function cilcked(){
-    nbr();
+function clicked(){    
     chg();
+    nbr();
 }
 function nbr(){
     let thing="block";
@@ -10,26 +10,37 @@ function nbr(){
     document.querySelector(".nav").style.display=thing;
 
 }
+let bo=true;
 function chg(){ 
-    // var trfrm=document.querySelector(".-top").style.transform=
-    // var ddsds= document.querySelector(".-top").style.marginBottom=
-    
-    //     if(document.querySelector(".-top").style.transform=="rotate(50deg)"&&document.querySelector(".-top").style.marginBottom == "50px"){
-           
-    //         document.querySelector(".-top").style.transition="0.5s"
-    //         document.querySelector(".-top").style.transitionTimingFuntion="ease-in-out"
-    //     }
-    //     if(document.querySelector(".-top").style.transform=="rotate(50deg)"&&document.querySelector(".-top").style.marginBottom == "50px"){
-    //         document.querySelector(".-top").style.transform="rotate(0deg)"
-    //         document.querySelector(".-top").style.marginBottom="0px"
-    //         document.querySelector(".-top").style.transition="0.5s"
-    //         document.querySelector(".-top").style.transitionTimingFuntion="ease-in-out"
-    //     }
+    if(bo){
     document.querySelector(".-top").setAttribute("style",
     "transform:rotate(50deg); transition:0.5s; margin-bottom:-7px;  transition-timing-function: ease-in-out;");
     document.querySelector(".-mid").style.display="none";
     document.querySelector(".-bot").setAttribute("style",
     "transform:rotate(-50deg); transition:0.5s;  margin-bottom:0px; transition-timing-function: ease-in-out; ");
-
-
+    bo=false;
+    }
+    else{
+        var tp=document.querySelector(".-top");
+        var md= document.querySelector(".-mid");
+        var bt= document.querySelector(".-bot");
+        if(tp.style.transform=="rotate(50deg)"&&tp.style.marginBottom=="-7px"){
+            tp.style.transform="rotate(0deg)";
+            tp.style.marginBottom="0px";
+            tp.style.transition="0.5s";
+            tp.style.transitionTimingFunction="ease-in-out";
+        }
+        if(bt.style.transform=="rotate(-50deg)"){
+            bt.style.transform="rotate(0deg)";
+            bt.style.transition="0.5s";
+            bt.style.transitionTimingFunction="ease-in-out";
+    
+        }
+        if(md.style.display=="none"){
+            md.style.display="block";
+    
+        }
+        bo=true;
+    }
+    
 }
